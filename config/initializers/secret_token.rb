33f575241744b7b6fe37c127337f953b34e8ebb4
Jessica <J_Saltz@hotmail.com>
2012-06-12 12:31:46 -0400

    'Rakefile'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackChannel::Application.config.secret_token = 'e8296c09a3c15354baa15c91735fb7bd32819a1f911a1fa639547103645b6720ec6ccabbd0cda05516e057c855b236b3f06e968de362e1457d1d67cf8a3d4312'
